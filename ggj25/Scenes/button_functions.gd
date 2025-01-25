f27f3b5a10ae45158247584894ba2ddfcf796049
WLGRest <78242454+WLGRest@users.noreply.github.com>
2025-01-25 17:57:32 -0500

    Settings

--- ggj25/Scenes/button_functions.gd
+++ ggj25/Scenes/button_functions.gd
@@ -2,3 +2,10 @@ extends Panel
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
+
+
+func _on_settings_button_pressed() -> void:
+	var settingspage = get_node("../../SettingsPage")
+	print(settingspage)
+	if settingspage:
+		settingspage.visible = not settingspage.visible
