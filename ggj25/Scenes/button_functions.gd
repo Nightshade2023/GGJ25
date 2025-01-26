1d377e3c7abce29ac44afaabe88194dfa297a97c
Nightshade2023 <150634815+Nightshade2023@users.noreply.github.com>
2025-01-26 01:03:07 -0500

    Audio Buses

--- ggj25/Scenes/button_functions.gd
+++ ggj25/Scenes/button_functions.gd
@@ -1,5 +1,9 @@
 extends Panel
 
+func _ready() -> void:
+	$"../../SettingsPage/Panel/musiclabel/musicbar".value = GameInfo.music_vol
+	$"../../SettingsPage/Panel/SFXlabel/SFXbar".value = GameInfo.sfx_vol
+
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
 
@@ -9,3 +13,15 @@ func _on_settings_button_pressed() -> void:
 	print(settingspage)
 	if settingspage:
 		settingspage.visible = not settingspage.visible
+
+
+func _on_musicbar_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		GameInfo.music_vol = $"../../SettingsPage/Panel/musiclabel/musicbar".value
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(GameInfo.music_vol))
+
+func _on_sf_xbar_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		GameInfo.music_vol = $"../../SettingsPage/Panel/musiclabel/musicbar".value
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(GameInfo.sfx_vol))
+		
---
+++ ggj25/Scripts/game_info.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var music_vol : float = 0.5
+var sfx_vol : float = 0.5
