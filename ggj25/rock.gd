e412aa035dd677d4b54bfd29981367945ed0215d
Nightshade2023 <150634815+Nightshade2023@users.noreply.github.com>
2025-01-26 06:37:10 -0500

    main scene quick fix

---
+++ ggj25/rock.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+@export var type : int = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if $CollisionShape2D
+	match type:
+		0:
+			$AnimatedSprite2D.animation = "small1"
+			$CollisionShape2D.scale.x = 1
+			$CollisionShape2D.scale.y = 1
+		1:
+			$AnimatedSprite2D.animation = "small2"
+			$CollisionShape2D.scale.x = 1.1
+			$CollisionShape2D.scale.y = 1
+		2:
+			$AnimatedSprite2D.animation = "medium1"
+			$CollisionShape2D.scale.x = 2.2
+			$CollisionShape2D.scale.y = 2
+		3:
+			$AnimatedSprite2D.animation = "medium2"
+			$CollisionShape2D.scale.x = 2.5
+			$CollisionShape2D.scale.y = 2
+		4:
+			$AnimatedSprite2D.animation = "large"
+			$CollisionShape2D.scale.x = 4.7
+			$CollisionShape2D.scale.y = 3.2
+			
